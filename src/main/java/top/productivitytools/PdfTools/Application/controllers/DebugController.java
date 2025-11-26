4f40aac2bb261e482a2e7ccd90273b7efb502085
Pawel Wujczyk <pwujczyk@gmail.com>
2025-11-26 21:36:26 +0100

    debug controller

---
+++ src/main/java/top/productivitytools/PdfTools/Application/controllers/DebugController.java
@@ -0,0 +1,17 @@
+package top.productivitytools.PdfTools.Application.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/debug")
+public class DebugController {
+
+    @GetMapping("/debug")
+    public String Hello(){
+        return "Hello World";
+    }
+}
