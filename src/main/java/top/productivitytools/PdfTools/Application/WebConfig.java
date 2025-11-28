7b67883318444e91ff9f66a79493021cf8ad4f06
Pawel Wujczyk <pwujczyk@gmail.com>
2025-11-28 23:03:08 +0100

    home service

---
+++ src/main/java/top/productivitytools/PdfTools/Application/WebConfig.java
@@ -0,0 +1,26 @@
+package top.productivitytools.PdfTools.Application;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:4200", "https://ptprojectsweb.web.app",
+                                "https://pt-habits.web.app")
+                        .allowCredentials(true)
+                        .exposedHeaders("Authorization");
+            }
+        };
+    }
+}
