f7a1566ab71ffc661ec941af34393385d63b05b4
Pawel Wujczyk <pwujczyk@gmail.com>
2025-12-08 22:58:56 +0100

    move to cloud run

---
+++ src/main/java/top/productivitytools/PdfTools/api/controllers/MergeOddEvenPagesController.java
@@ -0,0 +1,48 @@
+package top.productivitytools.PdfTools.api.controllers;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MergeOddEvenPagesController {
+
+    @QueryMapping
+    public String helloQuery() {
+        return "Hello World";
+    }
+
+    @MutationMapping
+    public String Hello(@Argument("name") String name) {
+        var response = "Hello " + name;
+        return response;
+    }
+
+    @PostMapping(value = "/mergeOddEvenPages", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Resource> mergeOddEvenPages(@RequestParam("file1") MultipartFile file1,
+            @RequestParam("file2") MultipartFile file2) throws IOException {
+
+        byte[] zipBytes = file1.getBytes();
+
+        ByteArrayResource resource = new ByteArrayResource(zipBytes);
+
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=merged.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .contentLength(zipBytes.length)
+                .body(resource);
+    }
+
+}
